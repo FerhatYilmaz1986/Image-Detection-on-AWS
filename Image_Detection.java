2d7cf070cd61e45e63e561947c0f262844490011
Fer <yilmazferhat1986@gmail.com>
2020-12-30 16:30:32 -0500

    Feature: Update image detection app

--- Image_Detection.java
+++ Image_Detection.java
@@ -17,3 +17,25 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+
+public class Label {
+
+	public static void main(String[] args)throws Exception {
+
+		String bucket = "bucketname";
+		String myqueue = "queuename";
+		// Read credentials from credential file
+		HashMap<String, String> credentials = new HashMap<String, String>();
+		try {
+			// URL url = Label.class.getClassLoader().getResource("credentials.txt");
+			// System.out.println(url);
+			// File myObj = new File(url.getPath());
+			File myObj = new File("cred.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String line = myReader.nextLine();
+				if(line != "") {
+					String[] data = line.split(" = ");
+					if(data.length == 2) {
+						credentials.put(data[0], data[1]);  
+					}   
