698811c74a1e937b90a6e80ceacd3ba395a393c8
Fer <yilmazferhat1986@gmail.com>
2021-01-02 09:00:49 -0500

    Feature: Update label detection app

--- Text_Detection.java
+++ Text_Detection.java
@@ -1,1 +1,8 @@
-//
+//Import libraries
+import com.Img.recognition.Label;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
